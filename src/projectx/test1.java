d28c08cf3f3aa49c6f46e74938c83ae38999df61
jonas hogrebe <hogrebejonas@icloud.com>
2025-10-21 11:17:05 +0200

    init

---
+++ src/projectx/test1.java
@@ -0,0 +1,13 @@
+package projectx;
+
+public class test1 {
+	static int a;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		a = 7;
+		System.out.println(a + 3);
+	}
+
+}
